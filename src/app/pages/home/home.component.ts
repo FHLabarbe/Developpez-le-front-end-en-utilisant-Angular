57f0d704134cf4d99c23ad9e82230f3791f7943c
FHLabarbe <94866592+FHLabarbe@users.noreply.github.com>
2024-08-16 21:15:01 +0200

    Etape 1 & 2

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from 'src/app/core/models/Olympic';
+
+// Interface pour les données du graphique ngx charts
+interface MedalData {
+  name: string; // Nom du pays
+  value: number; // Nombre total de médailles
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+export class HomeComponent implements OnInit, OnDestroy {
+  public olympics$: Observable<Olympic[]> = new Observable();
+  public isLoading: boolean = true;
+  public subscribeTest!: Subscription;
 
   constructor(private olympicService: OlympicService) {}
+  ngOnDestroy(): void {
+    this.subscribeTest.unsubscribe();
+  }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.loadMedalData();
   }
+
+  loadMedalData() {
+    this.olympics$ = this.olympicService.getOlympics();
+    this.subscribeTest = this.olympics$.subscribe(data => {
+      this.isLoading = false;
+    });
+  }
+  
 }
